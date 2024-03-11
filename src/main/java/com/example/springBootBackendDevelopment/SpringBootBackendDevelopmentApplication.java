bb8b24bc7f4533c5bb88274395cc7c7cd0ce357c
Melonhide <tt2830@columbia.edu>
2024-03-10 21:02:52 -0400

    Initial Commitment

---
+++ src/main/java/com/example/springBootBackendDevelopment/SpringBootBackendDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootBackendDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootBackendDevelopment/SpringBootBackendDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootBackendDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
